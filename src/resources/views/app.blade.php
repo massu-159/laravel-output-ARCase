8dd754d80a0c4eea04768d86e4c18de41520f90b
まっすー <mgajtwjp@gmail.com>
2022-09-30 19:36:38 +0900

    add ogp (twitter)

--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -5,6 +5,12 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta property="og:url" content="{{ url()->full() }}">
+  <meta property="og:title" content="ARCase">
+  <meta property="og:description" content="AR,VRの作品多数! 新しい発見がここにある!">
+  <meta property="og:site_name" content="ARCAse">
+  <meta property="og:image" content="{{ asset('img/ARCase.png') }}">
+  <meta name="twitter:card" content="summary" />
   <title>
     @yield('title')
   </title>
